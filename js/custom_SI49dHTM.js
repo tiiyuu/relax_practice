a14905463ba65a3ae7a0bfa3478fbf85cc437fa5
gces5816 <gces5816@gmail.com>
2021-06-05 01:28:33 +0800

    v3.0

---
+++ js/custom_SI49dHTM.js
@@ -0,0 +1,269 @@
+/* Slide 04 (#1) */
+/*
+         Eric
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣤⣶⣿⣿⣿⠿⠿⠿⣦⣤⣤⡀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣶⣿⣿⠟⠉⠁⠀⠀⠀⠀⠀⢻⣿⣿⣦⡀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀ ⣾⣿⣿⣿⡄⠀⠀⠀⠀⠀⠀⠀⠀⠰⣿⣿⣿⣷
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⣿⡿⢿⣷⣦⣤⣶⣶⡷⣶⣤⠹⣿⣿⣿⣷
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠸⣿⣿⠿⢿⣿⣿⢿⡏⠘⠿⣿⣶⣾⡿⠂⢻⣿⣿⣿
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⣯⡀⠀⠀⠀⢈⠀⠀⢀⠀⠀⠀⠀⠀⢸⣿⣿⡿
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣼⣿⣿⣷⡟⢿⣿⡿⢷⠞⠳⢄⣀⣤⣴⣾⣿⣿⠃
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⣿⣿⣶⣶⣦⣴⣶⣤⢹⣿⣿⣿⠟⠁
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢻⣿⣿⣾⣿⣿⣿⠟⠃⢉⣡⣥⣾⣿⠟⠁
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢹⣿⣿⣶⣤⣀⣀⣀⣀⣼⣿⣿⠋
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠶⢤⣀⡀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⣻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣃⠀⠀⠉⠛⠛⢟⣱⠶⢤⡀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣠⠴⠞⢿⣿⣿⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣾⣿⣷⠀⠀⠀⠀⠀⠀⠀⠀⠉⠳⢦
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⢤⡶⠛⠋⠀⠀⠀⠸⠿⣿⣿⣿⣿⣿⣿⣿⣿⡿⢛⣴⠟⠉⠙⠻⠆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠳⣄
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡤⠞⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⣿⢿⣿⣿⣿⣿⡿⢋⣴⠟⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⢷⡄
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⡰⠚⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣼⣿⣿⣿⢟⣵⠟⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢿⢆
+⠀⠀⠀⠀⠀⠀⠀⣠⠞⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⠟⡹⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⣳⡀
+⠀⠀⠀⠀⠀⠀⡴⢁⠞⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣟⠉⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⠀⠀⠀⠀⠀⠀⠀⠀⢈⣷⠄
+⠀⠀⠀⠀⠀⡸⡱⢋⡔⠃⠀⠈⣶⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⣷
+⠀⠀⠀⠀⢰⣟⡱⠃⠀⠀⠀⠀⣿⠀⣇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⣧⡀
+⠀⠀⠀⣰⣟⡜⠀⠀⠀⠀⠀⠀⣿⣼⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠸⡵⡄
+⠀⠀⢰⣿⣎⣀⣀⠀⠀⢠⠀⠀⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠸⡵⡄
+⠀⢠⡿⠋⠙⣿⣿⡆⠀⢹⢳⣤⣿⣿⡟⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣧⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠹⣽
+⠀⣾⠃⠀⠀⠈⢻⣿⠀⢸⣯⣿⣿⣿⡟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⠀⠀⠀⠀⠀⠀⠀⠀⡀⠀⠀⠀⠀⢻⡄
+⢰⣯⠰⡀⢤⡀⠀⠸⣧⣬⣿⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⣿⡄⠀⠀⢀⣄⠀⠀⠀⠀⠀⠀⠀⠀⠸⡇
+⣾⣿⣷⣿⣆⠙⠳⣄⡙⢿⣿⣿⣿⣿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⡇⠀⠀⡟⣿⠄⠊⠀⠀⠀⠀⠀⠄⠀⣷
+⠘⣿⣿⣿⣿⣆⠀⠈⣙⠾⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⡄⣰⢸⣿⠀⣀⣀⣀⡀⠀⠀⠀⠀⢹
+⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢻⣿⣿⡉⠉⣉⣻⠿⠿⠿⣿⣶⣤⣀⣸⡇
+⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠰⠋⣹⣿⣿⣿⣿⣷⣶⣀⣀⣀⣀⣀⠈⢻⡆
+⠀⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡀
+⠀⠀⠹⣿⣿⠹⣿⣿⣿⣿⣿⣿⣿⣣⠀⠀⠀⣀⣀⣀⣀⣀⣀⣤⣤⣀⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣨⣿⣿⣿⣿⢿⣿⣿⣿⣿⣿⣿⣿⣿
+⠀⠀⠀⠹⣿⣄⠘⣿⣿⣿⣿⣿⣟⣷⣿⣿⡿⠿⠿⠿⠿⠟⠛⠛⠛⠛⠿⠿⣿⣿⣷⣦⣤⣀⡀⣀⠀⠀⠀⠰⠋⠀⠀⠀⠀⠀⠀⠀⠀⣶⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟
+⠀⠀⠀⠀⣿⣿⣇⣿⣿⣿⣿⣿⣿⣧⣤⣤⣤⣤⣴⣶⣶⣷⣶⣿⣷⣶⣦⣤⣤⣤⣀⡈⠛⣿⣋⣻⣿⣷⣤⡀⠀⠀⠀⠀⠀⢀⣴⣿⣶⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠃
+⠀⠀⠀⠀⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣶⣯⣙⠿⢶⣷⣶⣶⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠋
+⠀⠀⠀⠀⠀⠈⣿⣿⣿⣳⣬⣽⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣏⣩⣥⣤⣭⡁⠀⢰⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠋
+⠀⠀⠀⠀⠀⠈⠻⣿⣿⣿⣷⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣟⣩⣥⣤⣤⣤⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⠉
+⠀⠀⠀⠀⠀⠀⠀⠈⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣾⣿⣿⣿⣿⣿⣿⣧⣼⣿⣿⣿⣿⣿⣿⣿⠿⠛⠛⠛⠛⠉⠁
+⠀⠀⠀⠀⠀⠀⠀⠀⠈⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠉
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡆
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠃
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠿⠿⠿⠿⠛⢿⡿⣿⣿⣿⣿
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⣿⠙⠓⡀⠀⠀⠀⠀⠀⠀⠀⠄⢧⣼⣿⣿⡄
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠿⣿⣶⣯⠭⠄⠀⠀⠀⠀⠀⠤⠲⢺⣿⣿⣿⣿
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⠿⠿⠿⠿⠶⣤⣄⡀⠀⠀⠀⠀⣀⣀⣴⣿⣿⣿⣿
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣶⣄⠀⠀⠀⣀⠴⠒⠋⠁⠀⠀⠁⢹⣿⣿⣿⠟
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡛⠿⣿⣿⣿⣟⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣼⣿⡿⠁
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣦⣄⣈⡙⠟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠐⣿⣿⠁
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢿⣷⣦⠀⠀⠀⠀⠀⠀⠀⠇⠀⠛⣿⡇
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣏⠉⠉⠀⢈⡀⠀⠀⠀⠀⠀⠀⠀⠄⣿⠧
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟⠄⠀⠀⢴⠀⠀⠀⠀⠁⠀⠀⠠⢠⣿⠌
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢿⣿⡇⠀⠀⠀⡀⠀⠀⠀⠀⢉⡁⠐⠂⢸⠛
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⡘⠂⣠⣤⣴⣿⠀⠀⠀⠀⣀⡇⠠⠀⠒
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡷⢆⣙⠛⠉⠀⠀⠀⣀⡾⣉⠃⢾
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀⣾⣿⠛⠆⣠⡶⠰
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣄⠀⠀⠀⠀⣼⣿⣿⣷⣬⣉⡰⠆
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⢻⡏⠀⠀⢰⣾⣿⡿⠿⠟⠆⡙⢷⡇
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣠⣾⣿⠋⠛⠛⠀⠀⢀⣁⢹⠃
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢨⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⣄⣀⠀⠀⢠⡃⠘⡁
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡍⠉⠻⡿⠿⠿⠿⢷⡦⢤⠤⡄
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⠿⠿⣿⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⡀⠀⠁⠀⠀⠀⠀⠀⠄⢳⡀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠸⣿⣶⣿⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣦⠀⠀⠀⠀⣀⡀⠘
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢘⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣦⡀⠀⠀⢡⣈⡀⠇
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣀⠀⠈⠙⢷
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⢾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣟⣣⡀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣴⢿⡆⠡
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣏⠉⠙⠢
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣇⡀⠆
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟⠂⠠
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠂
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠿⡿⠛⣿⣿⣿⣿⣿⡋
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⣿⣿⣿⣿⠻⠛⠛⠛⢿⡷⢤⣴⣶⣿⣿⣿⣿⣍⠉
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⣿⣷⣠⣤⣤⣴⣾⣿⣿⣿⣿⣿⣿⣿⣿⠃
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣽⣿⣿⣿⣿⣷⣌⡙⠛⠿⠿⣿⣿⣿⣿⣿⠿⠿⠃
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣼⣿⣿⣿⣿⣿⠻⠿⢷⣀⡀⠀⠀⠀⠉⠻⢷⠂
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣦⣤⣤⡴
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣼⣿⣿⣿⣿⠛⣻⣿⣿⠿⣿⣿⣿⣿⣿⣿⠇
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣽⡿⠛⠁
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠛⠛⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢹⣿⣿⣿⣿⣿⣿⣿⣿⠁
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⢿⣿⣿⣿⣿⣿⠟⠁
+*/
+var video1 = document.getElementById("relax");
+var progressBar_1 = document.getElementById("myBar_1");
+var bar_text_1 = document.getElementById("percentage_1");
+var timeStarted = 0;
+var timePlayed = 0;
+var duration = 0;
+var sentence = ["有時候，你必須按下暫停鍵，才能讓生活中大大小小的事進入腦中，慢慢體悟。\r\n——德國賽車選手 賽巴斯蒂安．維泰爾（Sebastian Vettel）",
+                "能在工作與玩樂之間找到平衡是重要的。\r\n——美國知名廚師 巴比．福雷（Bobby Flay）",
+               	"如果你想要放鬆，躺在草地看著雲朵飄過，或是坐在河邊看向遠方……。這些看似「無所事事」的事，才是真正能讓身體恢復活力的養分。\r\n——澳洲超級名模 米蘭達．寇兒（Miranda Kerr）",
+                "人們必須慢下腳步，並思考自己到底在做什麼。\r\n——英國演員麥可帕林（Michael Palin）",
+                "停下腳步並享受人生吧！步伐太快將使你錯失身旁美麗的景色，更可惜的是，你也可能因此失去思考人生意義的機會。\r\n——美國喜劇演員 埃迪．康托爾（Eddie Cantor）",
+                "當你沒空休息的時候，就是你該休息的時候了。\r\n——英國詩人 菲利普·西德尼（Philip Sidney）",
+                "閒暇不是心靈的充實，而是為了心靈得到休息。\r\n——古羅馬政治家 西塞羅（Marcus Tullius Cicero）",
+                "休息是為了走更長遠的路。",
+                "誰不會休息，誰就不會工作。\r\n——列寧",
+                "休息來自不眠不休，不眠不休來自休息。",
+                "休息是滋養疲乏的精神保姆。\r\n——莎士比亞（William Shakespeare）",
+                "休息與工作的關係，就如眼簾與眼睛的關係。\r\n——印度詩人 泰戈爾（Rabindranath Tagore）《飛鳥集》"
+               ];
+var encourage = ["再接再厲！",
+                 "加油，快完成了！",
+                 "繼續加油！",
+                 "它值得一試！",
+                 "繼續完成吧！",
+                 "不用擔心，你會變好的！",
+                 "我相信你！",
+                 "繼續試試看吧！",
+                 "Keep going!",
+                 "I’m with you!",
+                 "You are almost there!",
+                 "You’re on your way!",
+                 "Keep it up!"
+								];
+var bravo = ["很好！",
+             "太棒了！",
+             "非常好！",
+             "讓我給你個擁抱吧 : )",
+             "我欣賞你！",
+             "Bravo!!",
+             "Awesome!",
+             "獻上我的Respect!",
+             "做的好！",
+             "了不起！",
+             "恭喜你！",
+             "我以你為榮！",
+             "做得漂亮！",
+             "好極了！"
+						];
+document.getElementById("dialog_box").style.visibility = "hidden";
+// If video metadata is laoded get duration
+if(video1.readyState > 0)
+  getDuration.call(video1);
+//If metadata not loaded, use event to get it
+else
+{
+  video1.addEventListener('loadedmetadata', getDuration1);
+}
+// remember time user started the video
+function videoStartedPlaying1() {
+  timeStarted = new Date().getTime()/1000;
+}
+function videoStoppedPlaying1(event) {
+  // Start time less then zero means stop event was fired vidout start event
+  if(timeStarted>0) {
+    var playedFor = new Date().getTime()/1000 - timeStarted;
+    timeStarted = 0;
+    // add the new ammount of seconds played
+    timePlayed+=playedFor;
+  }
+  
+  var percentage = Math.floor(timePlayed/duration*100);
+  var status = "";
+  // Count as complete only if end of video was reached
+  if(timePlayed>=duration || event.type=="ended") {
+    percentage = 100;
+    // 100%'s feedbacks
+    var randomNumber = Math.floor(Math.random()*bravo.length);
+    status += bravo[randomNumber];
+    
+    // show a great quote
+    var randomNumber = Math.floor(Math.random()*sentence.length);
+    document.getElementById("dialog").innerHTML = sentence[randomNumber];
+    document.getElementById("dialog_box").style.visibility = "visible";
+  }
+  else {
+    // not 100%'s feedbacks
+    var randomNumber = Math.floor(Math.random()*encourage.length);
+    status += encourage[randomNumber];
+  }
+  progressBar_1.style.width = percentage+"%";
+  bar_text_1.innerHTML = percentage+"%";
+  document.getElementById("status").innerHTML = status;
+}
+function getDuration1() {
+  duration = video1.duration;
+  console.log("Duration: ", duration);
+}
+document.getElementById("x").onclick = function(){
+    timePlayed = 0;
+}
+document.getElementById("thumbnail").onclick = function(){
+  	timePlayed = 0;
+    document.getElementById("status").innerHTML = "";
+  	document.getElementById("dialog_box").style.visibility = "hidden";
+}
+video1.addEventListener("play", videoStartedPlaying1);
+video1.addEventListener("playing", videoStartedPlaying1);
+video1.addEventListener("ended", videoStoppedPlaying1);
+video1.addEventListener("pause", videoStoppedPlaying1);
+
+/* Slide 04 (#2) */
+var video = document.getElementById("video_2");
+var progressBar_2 = document.getElementById("myBar_2");
+var bar_text_2 = document.getElementById("percentage_2");
+var timeStarted = 0;
+var timePlayed = 0;
+var duration = 0;
+document.getElementById("quote_box2").style.visibility = "hidden";
+// If video metadata is laoded get duration
+if(video.readyState > 0)
+  getDuration.call(video);
+//If metadata not loaded, use event to get it
+else
+{
+  video.addEventListener('loadedmetadata', getDuration);
+}
+// remember time user started the video
+function videoStartedPlaying() {
+  timeStarted = new Date().getTime()/1000;
+}
+function videoStoppedPlaying(event) {
+  // Start time less then zero means stop event was fired vidout start event
+  if(timeStarted>0) {
+    var playedFor = new Date().getTime()/1000 - timeStarted;
+    timeStarted = 0;
+    // add the new ammount of seconds played
+    timePlayed+=playedFor;
+  }
+  
+  var percentage = Math.floor(timePlayed/duration*100);
+  var status = "";
+  // Count as complete only if end of video was reached
+  if(timePlayed>=duration || event.type=="ended") {
+    percentage = 100;
+    // 100%'s feedbacks
+    var randomNumber = Math.floor(Math.random()*bravo.length);
+    status += bravo[randomNumber];
+    
+    // show a great quote
+    var randomNumber = Math.floor(Math.random()*sentence.length);
+    document.getElementById("quote2").innerHTML = sentence[randomNumber];
+    document.getElementById("quote_box2").style.visibility = "visible";
+  }
+  else {
+    // not 100%'s feedbacks
+    var randomNumber = Math.floor(Math.random()*encourage.length);
+    status += encourage[randomNumber];
+  }
+  progressBar_2.style.width = percentage+"%";
+  bar_text_2.innerHTML = percentage+"%";
+  document.getElementById("played2").innerHTML = status;
+}
+function getDuration() {
+  duration = video.duration;
+  console.log("Duration: ", duration);
+}
+document.getElementById("close2").onclick = function(){
+    timePlayed = 0;
+}
+document.getElementById("cover2").onclick = function(){
+  	timePlayed = 0;
+    document.getElementById("played2").innerHTML = "";
+  	document.getElementById("quote_box2").style.visibility = "hidden";
+}
+video.addEventListener("play", videoStartedPlaying);
+video.addEventListener("playing", videoStartedPlaying);
+video.addEventListener("ended", videoStoppedPlaying);
+video.addEventListener("pause", videoStoppedPlaying);
